f7de660096b70880e0ccfed1ccff645ac02e9415
savyasanchi shukla <you@example.com>
2022-03-01 16:23:19 +0530

    comment placeholder fixed

--- client/src/components/post/CommentForm.js
+++ client/src/components/post/CommentForm.js
@@ -26,7 +26,7 @@ const CommentForm = (props) => {
           name="text"
           cols="30"
           rows="5"
-          placeholder="Create a post"
+          placeholder="Create a comment"
           required
           onChange={textChangeHandler}
           value={text}></textarea>
